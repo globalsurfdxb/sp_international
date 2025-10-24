c288235402f5c533c9dc3652cedec59662e78da0
rahees <raheez1995india@gmail.com>
2025-10-24 15:15:16 +0530

    sectr sldr

--- src/sections/home/SlideScrollThree.jsx
+++ src/sections/home/SlideScrollThree.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
-import { moveUp } from "../../motionVarients.js";
+
+
 
 import "swiper/css";
 import "swiper/css/pagination";
