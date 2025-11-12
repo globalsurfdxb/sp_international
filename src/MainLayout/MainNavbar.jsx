81d2fde3787825a4fad33fe744652cb3dc681eb8
rahees <raheez1995india@gmail.com>
2025-11-12 10:42:46 +0530

    pagin

--- src/MainLayout/MainNavbar.jsx
+++ src/MainLayout/MainNavbar.jsx
@@ -76,7 +76,7 @@ const MainNavbar = () => {
 
   return (
     <>
-      <nav className="pt-[16px] pb-[17px] border-b border-[#f0f0f0]">
+      <nav className="pt-[16px] pb-[17px] border-b border-[#f0f0f0] bg-[#ffffff]">
         <div className="container mx-auto px-4">
           <div className="flex items-center justify-between">
             {/* Logo */}
--- src/components/PressReleases/data.js
+++ src/components/PressReleases/data.js
@@ -101,6 +101,91 @@ export const pressReleases = {
         link:"/press-releases/press-release-12",
         category:"Project" 
       },
+      {
+        image:"./assets/images/press-releases/img-1.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling Commences Production",
+        date:"2024-02-14",
+        link:"/press-releases/press-release-1",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-2.jpg",
+        title:"Shapoorji Pallonji completes another iconic project - the BAPS Hindu Mandir in A...",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-2",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-3.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling V Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-3",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-4.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-4",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-5.jpg",
+        title:"Shapoorji Pallonji completes another iconic project - the BAPS Hindu Mandir in A...",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-5",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-6.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling V Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-6",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-7.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-7",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-8.jpg",
+        title:"Shapoorji Pallonji completes another iconic project - the BAPS Hindu Mandir in A...",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-8",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-9.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling V Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-9",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-10.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-10",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-11.jpg",
+        title:"Shapoorji Pallonji completes another iconic project - the BAPS Hindu Mandir in A...",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-11",
+        category:"Project" 
+      },
+      {
+        image:"./assets/images/press-releases/img-12.jpg",
+        title:"Shapoorji Pallonji Energy's FPSO Armada Sterling V Commences Production",
+        date:"2024-01-01",
+        link:"/press-releases/press-release-12",
+        category:"Project" 
+      },
+ 
       
     ]
 }
