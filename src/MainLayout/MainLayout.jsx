819e4ce11dd8eabe54d85eedd1437856ebe754f8
jishnum007 <jishnu@globalsurf.ae>
2025-11-25 10:59:54 +0530

    hide changes

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -11,9 +11,9 @@ import ProjectDetails from "../Pages/project-details/Index";
 import Projects from "../Pages/projects/Index";
 import NewsDetails from "../Pages/news-details/Index";
 import EngineeringConstruction from "../Pages/engineering-construction/Index";
-import Gallery from "../Pages/gallery/Index";
+// import Gallery from "../Pages/gallery/Index";
 // import QualitySafety from "../Pages/quality-and-safety/Index";
-import Contact from "../Pages/contact/Index";
+// import Contact from "../Pages/contact/Index";
 import CommunityEngagement from "../Pages/community-engagement/Index";
 const MainLayout = () => {
   return (
@@ -30,9 +30,9 @@ const MainLayout = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/news-details" element={<NewsDetails />} />
         <Route path="/engineering-construction" element={<EngineeringConstruction />} />
-        <Route path="/gallery" element={<Gallery />} />
+        {/* <Route path="/gallery" element={<Gallery />} /> */}
         {/* <Route path="/quality-and-safety" element={<QualitySafety />} />  */}
-        <Route path="/contact" element={<Contact />} /> 
+        {/* <Route path="/contact" element={<Contact />} />  */}
         <Route path="/community-engagement" element={<CommunityEngagement />} /> 
         <Route path="*" element={<Error />} />
       </Routes>
