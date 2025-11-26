6f9dbc6816184adc57dfb944376b2b8c6a0d5fad
jishnum007 <jishnu@globalsurf.ae>
2025-11-26 10:59:30 +0530

    contactus

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -12,7 +12,7 @@ import Projects from "../Pages/projects/Index";
 import NewsDetails from "../Pages/news-details/Index"; 
 import MEP from "../Pages/mep/Index";
 import Gallery from "../Pages/gallery/Index";  
-import Contact from "../Pages/contact/Index"; 
+import ContactUs from "../Pages/contact-us/Index"; 
 const MainLayout = () => {
   return (
     <BrowserRouter>
@@ -29,7 +29,7 @@ const MainLayout = () => {
         <Route path="/news-details" element={<NewsDetails />} /> 
         <Route path="/mep" element={<MEP />} />
         <Route path="/gallery" element={<Gallery />} />  
-        <Route path="/contact" element={<Contact />} />  
+        <Route path="/contact-us" element={<ContactUs />} />  
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
