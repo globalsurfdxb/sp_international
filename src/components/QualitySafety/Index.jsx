3daed1a5f964fc287a3cde70581cf39c435083a2
jishnum007 <jishnu@globalsurf.ae>
2025-11-26 11:55:27 +0530

    safty

--- src/components/QualitySafety/Index.jsx
+++ src/components/QualitySafety/Index.jsx
@@ -1,28 +1,28 @@
-import EnsuringSafety from "./sections/EnsuringSafety";
+ 
 import MainNavbar from "../../MainLayout/MainNavbar";
-import Footer from "../../MainLayout/Footer";
+import Footer from "../../MainLayout/Footer";  
 import Banner from "../../components/common/Banner"; 
-import CoreValues from "./sections/CoreValues";
-import Certifications from "./sections/Certifications";
-import SaftySlider from "./sections/SaftySlider";
-const Index = () => {
+import CoreValues from "../../components/QualitySafety/sections/CoreValues"
+import Certifications from "../../components/QualitySafety/sections/Certifications"
+import SaftySlider from "../../components/QualitySafety/sections/SaftySlider"
+import EnsuringSafety from "../../components/QualitySafety/sections/EnsuringSafety"
+const QualitySafety = () => {
   return (
     <>
-      <header>
+      <header className="">
         <MainNavbar />
+        <img src="./assets/images/shape-right.svg" alt="" className="absolute top-0 right-0 z-[-1]" />
       </header>
-      <main>
-        <Banner title="Quality & Safety" image="/assets/images/qualitysafety/qbanner.jpg" />
+       <Banner title="Quality & Safety" image="/assets/images/qualitysafety/qbanner.jpg" />
         <CoreValues /> 
         <Certifications />
         <SaftySlider />
-        <EnsuringSafety />  
-      </main>
+        <EnsuringSafety />
       <footer>
         <Footer />
       </footer>
     </>
   );
-};
+}
 
-export default Index;
+export default QualitySafety;
