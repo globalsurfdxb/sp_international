83a700bbf67b058137c1a1f0868cb670e438f81f
jishnum007 <jishnu@globalsurf.ae>
2025-11-26 11:24:32 +0530

    contactus

--- src/components/Contact/Index.jsx
+++ src/components/Contact/Index.jsx
@@ -2,7 +2,7 @@ import MainNavbar from "../../MainLayout/MainNavbar";
 import Footer from "../../MainLayout/Footer"; 
 import ContactDetails from "./sections/ContactDetails";
 import RegionalOffices from "./sections/RegionalOffices";
-const Index = () => {
+const ContactUs = () => {
   return (
     <>
       <header>
@@ -20,4 +20,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default ContactUs;
