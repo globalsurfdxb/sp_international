ba7136ad8d2a8479f616c9dcbf0c6e4fcc4082f5
jishnum007 <jishnu@globalsurf.ae>
2025-11-26 11:30:09 +0530

    contact removed

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -10,8 +10,7 @@ import PressReleases from "../Pages/press-releases/Index";
 import ProjectDetails from "../Pages/project-details/Index";
 import Projects from "../Pages/projects/Index";
 import NewsDetails from "../Pages/news-details/Index"; 
-import EngineeringConstruction from "../Pages/engineering-construction/Index";
-import Contact from "../Pages/contact/Index";
+import EngineeringConstruction from "../Pages/engineering-construction/Index"; 
 import MEP from "../Pages/mep/Index";
 import Gallery from "../Pages/gallery/Index";   
 const MainLayout = () => {
@@ -28,8 +27,7 @@ const MainLayout = () => {
         <Route path="/project-details" element={<ProjectDetails />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/news-details" element={<NewsDetails />} /> 
-        <Route path="/engineering-construction" element={<EngineeringConstruction />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/engineering-construction" element={<EngineeringConstruction />} /> 
         <Route path="/mep" element={<MEP />} />
         <Route path="/gallery" element={<Gallery />} />    
         <Route path="*" element={<Error />} />
