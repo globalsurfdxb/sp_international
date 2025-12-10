2362105d9954e70baaed3083262a819b100c9896
rahees <raheez1995india@gmail.com>
2025-12-10 10:57:40 +0530

    missing title overflow anims

--- src/components/ProjrectDetails/sections/MoreDetrails.jsx
+++ src/components/ProjrectDetails/sections/MoreDetrails.jsx
@@ -3,6 +3,7 @@ import { moredetrails } from "../data";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { moveUp, paragraphItem } from "../../../motionVarients";
 import { useRef } from "react";
+import H2Title from "../../../components/common/H2Title";
 const MoreDetrails = () => {
   const sectionRef = useRef(null);
 
@@ -20,7 +21,7 @@ const MoreDetrails = () => {
           <div className="2xl:max-w-[1008px] 3xl:max-w-[1208px] ml-auto">
             <div className="flex  justify-between items-center border-b border-black/20">
               <div className="relative z-20">
-                <motion.h2 variants={moveUp(0.3)} initial="hidden" whileInView="show" viewport={{ amount: 0.2, once: true }} className="text-60 font-light leading-[1.17] mb-3 lg:mb-7 text-black">{moredetrails.title}</motion.h2>
+                <H2Title titleText={moredetrails.title} marginClass="mb-3 lg:mb-7" />
                 <div>
                   {moredetrails.paragraphs.map((paragraph, index) => (
                     <motion.p key={index} variants={paragraphItem} initial="hidden" whileInView="show" viewport={{ amount: 0.2, once: true }} className="text-19 font-light text-paragraph mb-3 xl:mb-5  3xl:mb-[25.4px] last:lg:mb-10 last:2xl:mb-[80px]">{paragraph}</motion.p>
